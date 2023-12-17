999572486e61a63a3ee0f6fc06f859a0122f67fa
Kaan Düzbastılar <duzbastilarkaan@gmail.com>
2023-12-17 23:59:56 +0300

    Initial commit

---
+++ app/src/main/java/com/kaanduzbastilar/cryptocrazy/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.kaanduzbastilar.cryptocrazy.util
+
+object Constants {
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/cryptolist.json
+
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/crypto.json
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+    const val API_KEY = "atilsamancioglu/IA32-CryptoComposeData/main/cryptolist.json"
+
+}
